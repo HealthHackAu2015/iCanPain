75c974cf5c832553bcafb1fd8ae1bb483f39d2f8
Glen Arrowsmith <glen.arrowsmith@gmail.com>
2015-10-24 00:41:41 +1000

    Final commit

--- www/js/app.js
+++ www/js/app.js
@@ -40,12 +40,12 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
   // Each tab has its own nav history stack:
 
-  .state('tab.dash', {
-    url: '/dash',
+  .state('tab.goals', {
+    url: '/goals',
     views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
+      'tab-goals': {
+        templateUrl: 'templates/tab-goals.html',
+        controller: 'GoalsCtrl'
       }
     }
   })
@@ -80,6 +80,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/tab/goals');
 
 });
