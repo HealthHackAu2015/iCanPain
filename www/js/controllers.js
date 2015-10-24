9ad0ba920e7e182ce9dd890141465be21569ea85
Glen Arrowsmith <glen.arrowsmith@gmail.com>
2015-10-24 16:59:23 +1000

    Removed obsolete inputs

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,92 @@
 angular.module('starter.controllers', [])
 
-.controller('GoalsCtrl', function($scope) {
+.controller('GoalsCtrl', function($scope) {})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+
+  $scope.$on('$ionicView.enter', function(e) {
+    $scope.initChart();
+  });
+
+  $scope.initChart = function() {
+    $(function() {
+      $.getJSON('http://www.highcharts.com/samples/data/jsonp.php?filename=usdeur.json&callback=?', function(data) {
+
+        $('#container').highcharts({
+          chart: {
+            zoomType: 'x'
+          },
+          title: {
+            text: 'USD to EUR exchange rate over time'
+          },
+          subtitle: {
+            text: document.ontouchstart === undefined ?
+              'Click and drag in the plot area to zoom in' : 'Pinch the chart to zoom in'
+          },
+          xAxis: {
+            type: 'datetime'
+          },
+          yAxis: {
+            title: {
+              text: 'Exchange rate'
+            }
+          },
+          legend: {
+            enabled: false
+          },
+          plotOptions: {
+            area: {
+              fillColor: {
+                linearGradient: {
+                  x1: 0,
+                  y1: 0,
+                  x2: 0,
+                  y2: 1
+                },
+                stops: [
+                  [0, Highcharts.getOptions().colors[0]],
+                  [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                ]
+              },
+              marker: {
+                radius: 2
+              },
+              lineWidth: 1,
+              states: {
+                hover: {
+                  lineWidth: 1
+                }
+              },
+              threshold: null
+            }
+          },
+
+          series: [{
+            type: 'area',
+            name: 'USD to EUR',
+            data: data
+          }]
+        });
+      });
+    });
+  };
+
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('QuestionsCtrl', function($scope) {
+  $scope.$on('$ionicView.enter', function(e) {
+    var vertRanges = document.getElementsByClassName("range-vertical");
+    for (var i = 0; i < vertRanges.length; ++i) {
+      var slider = vertRanges[i];
+      slider.style.width = slider.clientHeight + "px";
+      slider.style["margin-left"] = (-slider.clientHeight / 2) + "px";
+    }
+  });
+
   $scope.values = {
     pain: window.localStorage.pain || 5,
     productivity: window.localStorage.productivity || 5,
@@ -28,94 +114,6 @@ angular.module('starter.controllers', [])
       $scope.saveButtonText = 'Save';
       $scope.$apply();
     }, 2000);
-  };
-
-})
-
-.controller('ChatsCtrl', function($scope, Chats) {
-
-  $scope.$on('$ionicView.enter', function(e) { $scope.initChart(); });
-
-  $scope.initChart = function () {
-    $(function () {
-        $.getJSON('http://www.highcharts.com/samples/data/jsonp.php?filename=usdeur.json&callback=?', function (data) {
-
-            $('#container').highcharts({
-                chart: {
-                    zoomType: 'x'
-                },
-                title: {
-                    text: 'USD to EUR exchange rate over time'
-                },
-                subtitle: {
-                    text: document.ontouchstart === undefined ?
-                            'Click and drag in the plot area to zoom in' : 'Pinch the chart to zoom in'
-                },
-                xAxis: {
-                    type: 'datetime'
-                },
-                yAxis: {
-                    title: {
-                        text: 'Exchange rate'
-                    }
-                },
-                legend: {
-                    enabled: false
-                },
-                plotOptions: {
-                    area: {
-                        fillColor: {
-                            linearGradient: {
-                                x1: 0,
-                                y1: 0,
-                                x2: 0,
-                                y2: 1
-                            },
-                            stops: [
-                                [0, Highcharts.getOptions().colors[0]],
-                                [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
-                            ]
-                        },
-                        marker: {
-                            radius: 2
-                        },
-                        lineWidth: 1,
-                        states: {
-                            hover: {
-                                lineWidth: 1
-                            }
-                        },
-                        threshold: null
-                    }
-                },
-
-                series: [{
-                    type: 'area',
-                    name: 'USD to EUR',
-                    data: data
-                }]
-            });
-        });
-    });
-}
-
-})
-
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
-
-.controller('QuestionsCtrl', function ($scope) {
-	$scope.$on('$ionicView.enter', function (e) {
-		var vertRanges = document.getElementsByClassName("range-vertical");
-		for (var i = 0; i < vertRanges.length; ++i) {
-			var slider = vertRanges[i];
-			slider.style.width = slider.clientHeight + "px";
-			slider.style["margin-left"] = (-slider.clientHeight / 2) + "px";
-		}
-	});
-  $scope.save = function() {
-    // alert('saved');
   };
 })
 
